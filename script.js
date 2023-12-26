31a1668e31892cf1aa9ab2be829703d1456de4a9
ykamenashi <kamenashi@cloud-ace.jp>
2023-12-26 20:53:09 +0900

    QuickFix: 余計なクエリ文字列を削除

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener((response, sender, sendResponse)=>{
         (
             function(s,e,d){
                 open(
-                    "https://"+ d +"/share?original_referer=javascript:close&text="+e(document.title)+" "+e(location.href),"_blank","width=550,height=420,left="+(s.availLeft+s.availWidth/2-275)+",top="+(s.availTop+s.availHeight/2-210)
+                    "https://"+ d +"/share?text="+e(document.title)+" "+e(location.href),"_blank","width=550,height=420,left="+(s.availLeft+s.availWidth/2-275)+",top="+(s.availTop+s.availHeight/2-210)
                 );
             }
         )(screen,encodeURIComponent,misskey_domain_str)
