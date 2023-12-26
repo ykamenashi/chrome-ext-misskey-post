9f6ab46a5f57ffb7761e5e530b903a9ea4e63512
ykamenashi <kamenashi@cloud-ace.jp>
2023-12-26 19:59:21 +0900

    設定画面から投稿先ドメイン名を指定できるようになった。

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
 chrome.action.onClicked.addListener((tab)=>{
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ['script.js']
+    let misskey_domain = 'misskey.io';
+    let rtnPromise = chrome.storage.sync.get( { misskey_domain: 'misskey.io' }, function(result){
+        misskey_domain = result.misskey_domain;
+        chrome.tabs.sendMessage(tab.id, {
+                message: "PostToMisskey",
+                domain: misskey_domain
+        });
+
     });
+    rtnPromise
+        .then((res)=>{
+        })
+        .catch((err)=>{
+        })
 });
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+chrome.runtime.onMessage.addListener((response, sender, sendResponse)=>{
+    if( response.message == "PostToMisskey" ){
+        let misskey_domain_str = response.domain;
 
-(
-    function(s,e){
-        open(
-            "https://misskey.io/share?original_referer=javascript:close&text="+e(document.title)+" "+e(location.href),"_blank","width=550,height=420,left="+(s.availLeft+s.availWidth/2-275)+",top="+(s.availTop+s.availHeight/2-210)
-        );
+        (
+            function(s,e,d){
+                open(
+                    "https://"+ d +"/share?original_referer=javascript:close&text="+e(document.title)+" "+e(location.href),"_blank","width=550,height=420,left="+(s.availLeft+s.availWidth/2-275)+",top="+(s.availTop+s.availHeight/2-210)
+                );
+            }
+        )(screen,encodeURIComponent,misskey_domain_str)
+
     }
-)(screen,encodeURIComponent)
+})
