2058316e25fc4c415c64cdb797afa7134d6916f0
ykamenashi <kamenashi@cloud-ace.jp>
2024-08-25 15:09:53 +0900

    promiseのエラーが出ないように修正

--- background.js
+++ background.js
@@ -1,15 +1,10 @@
-chrome.action.onClicked.addListener((tab)=>{
+chrome.action.onClicked.addListener( async (tab)=>{
     let misskey_domain = 'misskey.io';
-    let rtnPromise = chrome.storage.sync.get( { misskey_domain: 'misskey.io' }, function(result){
+    let rtnPromise = await chrome.storage.sync.get( { misskey_domain: 'misskey.io' }, function(result){
         misskey_domain = result.misskey_domain;
         chrome.tabs.sendMessage(tab.id, {
                 message: "PostToMisskey",
                 domain: misskey_domain
         });
     });
-    rtnPromise
-        .then(()=>{
-        })
-        .catch((err)=>{
-        })
 });
